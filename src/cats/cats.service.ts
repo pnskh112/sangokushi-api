ba6138bc2577a97962416a6297a80969ed930b9b
pnskh112 <pnskh112pnskh112pnskh112@gmail.com>
2021-02-12 00:23:27 +0900

    add:cats.controller.ts

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,6 +1,31 @@
 import { Injectable } from "@nestjs/common";
+import { NotFoundException } from "@nestjs/common/exceptions";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Cats } from "dist/cats/cats.model";
+import { CatsRepository } from '../repository/cats.repository';
+import { Cat } from './cats.entity';
 
 @Injectable()
 export class CatsService {
+    constructor(
+        @InjectRepository(CatsRepository)
+        private catsRepository: CatsRepository,
+    ) {}
+    private cats: Cats[] = [];
 
+    async getCatsById(id: number): Promise<Cat> {
+        const found = await this.catsRepository.findOne(id);
+
+        if (!found) {
+            throw new NotFoundException(`Cat with ID ${id} not found`);
+        }
+
+        return found;
+    }
+
+
+
+    getAllCats(): Cats[] {
+        return this.cats;
+    }
 }
--- src/usecase/cats/cats.controller.ts
+++ src/usecase/cats/cats.controller.ts
@@ -1,9 +1,31 @@
+import { Body, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { Controller, Get } from '@nestjs/common';
+import { Cats } from 'src/cats/cats.model';
+import { CatsService } from '../../cats/cats.service';
 
 @Controller('cats')
 export class CatsController {
+    constructor(private catsService: CatsService) {}
+
     @Get()
-    findAll(): String {
-        return 'This action returns all cats';
+    getAllCats(): Cats[] {
+        return this.catsService.getAllCats();
+    }
+
+    @Get('/:id')
+    getCatsById(
+        @Param('id', ParseIntPipe) id: number
+    ): Promise<Cats> {
+        return this.catsService.getCatsById(id);
+    }
+
+    // @Get()
+    // findAll(): String {
+    //     return 'This action returns all cats';
+    // }
+
+    @Post()
+    createCats(@Body() body) {
+        console.log('body',body);
     }
 }
